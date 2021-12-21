af021ba9d0e7a23d1ba86fc75a4f8636ce252697
Muhammad Ehsaan <ehsaan2611@gmail.com>
2021-12-21 14:02:31 +0500

    stack

---
+++ Stack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VivaProject
+{
+    
+    internal class Stack<T>
+    {
+        private int top;
+        private List<T> Arr;
+        public Stack()
+        {
+            Arr = new List<T>();
+            top = -1;
+
+        }
+        public void Push(T n)
+        {
+            Arr.Add(n);
+            top++;
+        }
+        public T Pop()
+        {
+
+            return Arr[top];
+        }
+
+    }
+}
